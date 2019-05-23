fb6d0ad7bc822d46f521d2f67860ae595756b285
Max Williams <mw12315@bc4login3.bc4.acrc.priv>
2019-05-23 13:08:35 +0100

    output the output directory to sdtout and add time take for scan to output

--- scan_energies/find_scan_energies.py
+++ scan_energies/find_scan_energies.py
@@ -8,13 +8,21 @@ else:
 
 
 print("Scan Energies for {}".format(fileIn))
-print("{:5s}\t{:19s}\t{:6}".format("SCAN","Energy / AU","Points"))
+print("{:5s}\t{:19s}\t{:6}\t{:10}".format("SCAN","Energy / AU","Points","Time / min"))
 
+last_time = 0
 with open(fileIn,'r') as f:
     o = open("{}_Escan.csv".format(fileIn),'w')
     o.write("{:5s},{:19s},{:6}\n".format("SCAN","Energy / AU","Points"))
     nScans = 0
     for line in f:
+        if "OPTI        TELAPSE" in line:
+            splitLine = line.strip().split()
+            try:
+                te_lapse = float(splitLine[3])
+            except ValueError:
+                print(splitline)
+
         if "OPT END - CONVERGED" in line:
             nScans += 1
             splitLine = line.strip().split()
@@ -26,6 +34,8 @@ with open(fileIn,'r') as f:
                 except ValueError:
                     pass
 
-            print("{:5}\t{:15.10e}\t{:6d}".format(nScans,float(nums[0]),int(nums[1])))
-            o.write("{:5},{:15.10e},{:6d}\n".format(nScans,float(nums[0]),int(nums[1])))
+            print("{:5}\t{:15.10e}\t{:6d}\t{:10.0f}".format(nScans,float(nums[0]),int(nums[1]),round((te_lapse - last_time)/60,0)))
+            o.write("{:5},{:15.10e},{:6d}\t{:10.0f}\n".format(nScans,float(nums[0]),int(nums[1]),round((te_lapse - last_time)/60,0)))
 
+print("Output to: {}".format(o.name))
+o.close()
