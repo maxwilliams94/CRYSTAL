481d78d475bdd0a0909a9e7b2729dcd6e5d84626
Max Williams <mw12315@bristol.ac.uk>
2019-05-24 09:49:27 +0100

    add printing/output of minutes per scan

--- scan_energies/find_scan_energies.py
+++ scan_energies/find_scan_energies.py
@@ -8,7 +8,7 @@ else:
 
 
 print("Scan Energies for {}".format(fileIn))
-print("{:5s}\t{:19s}\t{:6}\t{:10}".format("SCAN","Energy / AU","Points","Time / min"))
+print("{:5s}\t{:19s}\t{:6}\t{:10}\t{:10}".format("SCAN","Energy / AU","Points","Time / min", "min/scan"))
 
 last_time = 0
 with open(fileIn,'r') as f:
@@ -16,7 +16,7 @@ with open(fileIn,'r') as f:
     o.write("{:5s},{:19s},{:6}\n".format("SCAN","Energy / AU","Points"))
     nScans = 0
     for line in f:
-        if "OPTI        TELAPSE" in line:
+        if "TELAPSE" in line:
             splitLine = line.strip().split()
             try:
                 te_lapse = float(splitLine[3])
@@ -34,8 +34,9 @@ with open(fileIn,'r') as f:
                 except ValueError:
                     pass
 
-            print("{:5}\t{:15.10e}\t{:6d}\t{:10.0f}".format(nScans,float(nums[0]),int(nums[1]),round((te_lapse - last_time)/60,0)))
-            o.write("{:5},{:15.10e},{:6d}\t{:10.0f}\n".format(nScans,float(nums[0]),int(nums[1]),round((te_lapse - last_time)/60,0)))
+            print("{:5}\t{:15.10e}\t{:6d}\t{:10.1f}\t{:10.1f}".format(nScans,float(nums[0]),int(nums[1]),round(te_lapse/60,1),round((te_lapse - last_time)/60,1)))
+            o.write("{:5},{:15.10e},{:6d}\t{:10.1f}\t{:10.1f}\n".format(nScans,float(nums[0]),int(nums[1]),round(te_lapse/60,1),round((te_lapse - last_time)/60,1)))
+            last_time = te_lapse
 
 print("Output to: {}".format(o.name))
 o.close()
